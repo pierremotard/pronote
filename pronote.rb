2e1932b527b973f041932d3bf2c5e9439fd10558
Pierre Motard <pierre.motard99@gmail.com>
2015-04-19 11:26:29 +0200

    Initial : Welcome & Ask user's status
    Demande à l'utilisateur s'il est prof ou eleve et renvoie à une méthode selon le statut

---
+++ pronote.rb
@@ -0,0 +1,11 @@
+def pronote
+  puts "Bienvenue sur le logiciel Pronote !"
+  print "Professeur ou eleve ?"
+  statut = gets.chomp
+  if statut == "professeur"
+    load 'pronote_professeur.rb'
+  elsif statut == "eleve"
+    load 'pronote_eleve.rb'
+  else
+    puts "Statut inexistant"
+
